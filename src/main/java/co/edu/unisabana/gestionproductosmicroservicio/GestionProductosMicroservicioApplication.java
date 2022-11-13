5f76d877d78b82bd064007a9b27835ae981e7081
rximenars <ximis.ximena@gmail,com>
2022-11-13 00:01:16 -0500

    Spring del proyecto, base

---
+++ src/main/java/co/edu/unisabana/gestionproductosmicroservicio/GestionProductosMicroservicioApplication.java
@@ -0,0 +1,13 @@
+package co.edu.unisabana.gestionproductosmicroservicio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionProductosMicroservicioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionProductosMicroservicioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/edu/unisabana/gestionproductosmicroservicio/GestionProductosMicroservicioApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.unisabana.gestionproductosmicroservicio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionProductosMicroservicioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
