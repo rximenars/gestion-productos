cba11b88cc3337d505f58d78e0075263cd9dd090
ferchoferfernny <fercho.fer.fernny@hotmail.com>
2022-11-14 15:07:46 -0500

    primer commit de seguridad

--- src/main/java/co/edu/unisabana/gestionproductosmicroservicio/SecurityConfiguration.java
+++ src/main/java/co/edu/unisabana/gestionproductosmicroservicio/SecurityConfiguration.java
@@ -1,6 +1,9 @@
 package co.edu.unisabana.gestionproductosmicroservicio;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 
 @Configuration
 
